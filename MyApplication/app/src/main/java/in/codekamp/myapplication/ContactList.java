73b1ab86ca3e33bd0ec1c65bcd50481545678036
Prashant Chaudhary <101.prashant@gmail.com>
2016-07-05 09:59:02 +0530

    Retrofit demo

---
+++ MyApplication/app/src/main/java/in/codekamp/myapplication/ContactList.java
@@ -0,0 +1,57 @@
+
+package in.codekamp.myapplication;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class ContactList {
+
+    @SerializedName("id")
+    @Expose
+    private String id;
+
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+
+    /**
+     * 
+     * @return
+     *     The id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * 
+     * @param id
+     *     The id
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+
+    /**
+     * 
+     * @return
+     *     The name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 
+     * @param name
+     *     The name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- MyApplication/app/src/main/java/in/codekamp/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/in/codekamp/myapplication/MainActivity.java
@@ -2,12 +2,58 @@ package in.codekamp.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity implements Callback<ListResponse> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://us11.api.mailchimp.com/2.0/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        MailchimpService mailchimpService = retrofit.create(MailchimpService.class);
+
+        Call<ListResponse> call = mailchimpService.fetchLists("dc00610cdef48bd9d76a5f83e4e90415-us11");
+        call.enqueue(this);
+
+        Call<ContactResponse> call1 = mailchimpService.fetchContacts("dc00610cdef48bd9d76a5f83e4e90415-us11", "55ec6cd534");
+
+        call1.enqueue(new Callback<ContactResponse>() {
+            @Override
+            public void onResponse(Call<ContactResponse> call, Response<ContactResponse> response) {
+                for (int i = 0; i < response.body().getContacts().size(); i++) {
+                    Log.d("CodeKamp", response.body().getContacts().get(i).getProfile().getFirstName());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ContactResponse> call, Throwable t) {
+                Log.d("CodeKamp", t.getMessage());
+                t.printStackTrace();
+            }
+        });
+    }
+
+    @Override
+    public void onResponse(Call<ListResponse> call, Response<ListResponse> response) {
+        for (int i = 0; i < response.body().getContactLists().size(); i++) {
+            Log.d("CodeKamp", response.body().getContactLists().get(0).getId());
+        }
+    }
+
+    @Override
+    public void onFailure(Call<ListResponse> call, Throwable t) {
+
     }
 }
